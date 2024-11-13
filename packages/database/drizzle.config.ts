a500f6f7ef4a33343b80beb09360ebd63c9a3115
Anirudh Sriram <hello@techwithanirudh.com>
2024-11-13 11:10:11 +0000

    fix: drizzle out

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './src/schema.ts',
-  out: './migrations',
+  out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: env.DATABASE_URL,
