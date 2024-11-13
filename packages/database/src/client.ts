b5c2ec94fd08943985dc8cb448c5d6e95166399d
Anirudh Sriram <hello@techwithanirudh.com>
2024-11-13 11:09:06 +0000

    fix: neon http

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,7 +1,7 @@
 import 'server-only';
 
 import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import { env } from '@repo/env';
 
 const sql = neon(env.DATABASE_URL);
