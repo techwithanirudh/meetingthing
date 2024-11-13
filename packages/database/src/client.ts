4b63c58a8cab1eefa8ac8b4f77dc658ff5e8660d
Anirudh Sriram <hello@techwithanirudh.com>
2024-11-13 11:12:04 +0000

    fix: schema

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -3,6 +3,7 @@ import 'server-only';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { env } from '@repo/env';
+import * as schema from './schema';
 
 const sql = neon(env.DATABASE_URL);
-export const database = drizzle({ client: sql });
+export const database = drizzle({ client: sql, schema });
