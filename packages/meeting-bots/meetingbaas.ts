97d70bc360528ae1b8f0ff48b394469907f9d71b
Anirudh Sriram <hello@techwithanirudh.com>
2024-11-15 08:59:54 +0000

    fix: type

--- packages/meeting-bots/meetingbaas.ts
+++ packages/meeting-bots/meetingbaas.ts
@@ -1,6 +1,6 @@
 import { env } from '@repo/env';
 import ky from 'ky';
-import type { RecordMeetingResponse } from './types/meetingbaas.js';
+import type { RecordMeetingResponse } from './types/meetingbaas';
 
 // biome-ignore lint/style/useNamingConvention: url seems nice with caps
 export const recordMeeting = async (meetingURL: string, botName?: string) => {
