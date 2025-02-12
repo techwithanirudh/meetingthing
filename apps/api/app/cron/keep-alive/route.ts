39079de369af2349952ae07c33d69478d6885b5f
Anirudh Sriram <hello@techwithanirudh.com>
2025-02-12 13:53:17 +0000

    fix: use cron

--- apps/api/app/cron/keep-alive/route.ts
+++ apps/api/app/cron/keep-alive/route.ts
@@ -1,22 +1,3 @@
-// import { eq } from '@repo/database';
-// import { database } from '@repo/database/client';
-// import { meetingsTable } from '@repo/database/schema';
-
 export const POST = () => {
-  // const newPage = await database
-  //   .insert(meetingsTable)
-  //   .values({
-  //     name: 'Impromptu Meeting',
-  //     provider: 'meetingbaas',
-  //     status: 'loading',
-  //   })
-  //   .returning({
-  //     id: meetingsTable.id,
-  //   });
-
-  // await database
-  //   .delete(meetingsTable)
-  //   .where(eq(meetingsTable.id, newPage[0].id));
-
   return new Response('OK', { status: 200 });
 };
