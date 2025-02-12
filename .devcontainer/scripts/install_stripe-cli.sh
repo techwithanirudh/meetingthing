dfab73a4e624cfc35128383193ac853a3a63a889
Anirudh Sriram <hello@techwithanirudh.com>
2025-02-12 06:09:15 +0000

    feat: devcontainers

---
+++ .devcontainer/scripts/install_stripe-cli.sh
@@ -0,0 +1,4 @@
+curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | sudo tee /usr/share/keyrings/stripe.gpg
+echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | sudo tee -a /etc/apt/sources.list.d/stripe.list
+sudo apt update
+sudo apt install stripe -y
---
+++ .devcontainer/scripts/install_vercel-cli.sh
@@ -0,0 +1,1 @@
+pnpm install -g vercel
